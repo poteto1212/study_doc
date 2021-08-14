32c3d949456adc7c05e3b0ef9c6293576fedc7f4
Your Name <you@example.com>
2021-08-14 14:52:15 +0000

    todoテストの記載まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_14_141735) do
+
+  create_table "todos", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "is_completed", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
